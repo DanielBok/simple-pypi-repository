8fcfaa67963a051a39ef41f5ca25280f0346d3bd
Daniel Bok <daniel.bok@outlook.com>
2020-01-31 22:58:46 +0800

    Add package upload route and handler

--- server/application/routes/packages/operations.py
+++ server/application/routes/packages/operations.py
@@ -1,7 +1,26 @@
+import os
 from pathlib import Path
 
-from flask import abort, request, render_template
+from flask import abort, render_template, request
 from werkzeug.datastructures import FileStorage
+
+from application.models import Account
+
+
+def get_account():
+    if request.authorization is None:
+        abort(403, "No credentials provided")
+
+    username = request.authorization['username']
+    password = request.authorization['password']
+    if username == "" or password == "":
+        abort(403, "No credentials provided. Please ensure you have a registered account")
+
+    account = Account.find_account(username, password)
+    if account is None:
+        abort(403, "Invalid account credentials.Please ensure you have a valid registered account")
+
+    return account
 
 
 class GetHandler:
@@ -23,17 +42,16 @@ class GetHandler:
 
 
 class PostHandler:
-    def __init__(self, package_folder: str):
-        package_folder = package_folder.strip()
-        if package_folder == "":
+    def __init__(self, folder: str, name: str):
+        folder = folder.strip()
+        if folder == "":
             raise ValueError("upload folder value cannot be an empty string")
 
-        self.package_folder = Path(package_folder)
+        self.package_folder = Path(folder).joinpath(name)
         if not self.package_folder.exists():
             self.package_folder.mkdir(600, True, True)
 
-    def upload_package(self):
-        pkg_root = self.package_folder.parent.joinpath("packages")
+    def upload_package(self, allow_override: bool):
         if 'content' not in request.files:
             abort(400, "No content found")
 
@@ -41,14 +59,13 @@ class PostHandler:
         if not file.filename.endswith(".tar.gz") and not file.filename.endswith('.whl'):
             abort(400, "Invalid file format. Package build package with sdist or wheel")
 
-        name = request.form.get('name', None)
-        if name is None:
-            abort(400, 'Package name is not specified')
-
-        pkg_path = pkg_root.joinpath(name, file.filename)
+        pkg_path = self.package_folder.joinpath(file.filename)
         if pkg_path.exists():
-            abort(400, "Package already exists. To force an overwrite, use the '-f' flag in twine or "
-                       "set force to True in the form post")
+            if not allow_override:
+                abort(400, "Package already exists and cannot be overwritten. To enable overwrite, set "
+                           "the allow_override option to True via the api")
+            else:
+                os.remove(pkg_path.as_posix())
 
         file.save(pkg_path.as_posix())
         return "Okay", 200
--- server/application/routes/packages/routes.py
+++ server/application/routes/packages/routes.py
@@ -1,20 +1,35 @@
-from flask import Blueprint, current_app, abort, request
+from flask import Blueprint, abort, current_app, request
 
-from .operations import GetHandler, PostHandler
+from application.models import Package
+from .operations import GetHandler, PostHandler, get_account
 
 bp = Blueprint("packages", __name__, template_folder="templates")
 
 
-@bp.route('/', defaults={'path': ''}, methods=["GET", "POST", "DELETE"])
-@bp.route('/<path:path>', methods=["GET", "POST", "DELETE"])
+@bp.route('/', defaults={'path': ''}, methods=["GET"])
+@bp.route('/<path:path>', methods=["GET"])
 def index(path):
     folder = current_app.config['PACKAGE_FOLDER']
-    if request.method == "GET":
-        handler = GetHandler(folder)
-        if path == '':
-            return handler.render_index()
+    handler = GetHandler(folder)
+    if path == '':
+        return handler.render_index()
+    else:
+        abort(404, "Not implemented")
 
-    if request.method == "POST":
-        return PostHandler(folder).upload_package()
 
-    return abort(405, "")
+@bp.route('/', methods=['POST'])
+def upload_package():
+    account = get_account()
+    if (package_name := request.values.get('name', '').strip()) == '':
+        abort(400, 'package name not provided')
+
+    package = Package.find_by_name(package_name)
+    if package is None:
+        package = Package(package_name, False, False)
+        account.packages.append(package)
+        account.save()
+    elif package.account != account:
+        abort(403, "Invalid credentials. Please ensure that you have the right credentials")
+
+    folder = current_app.config['PACKAGE_FOLDER']
+    return PostHandler(folder, package_name).upload_package(package.allow_override)
