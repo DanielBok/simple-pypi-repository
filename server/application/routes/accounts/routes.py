be72de16ef5f421f54ecbb60f904a6266c431af5
Daniel Bok <daniel.bok@outlook.com>
2020-01-31 22:39:24 +0800

    Remove redundant jsonify

--- server/application/routes/accounts/routes.py
+++ server/application/routes/accounts/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, current_app, jsonify, request
+from flask import Blueprint, abort, current_app, request
 
 from application.models import Account
 
@@ -15,7 +15,7 @@ def create_account():
         abort(400, f"Email '{data['email']}' is already taken")
 
     try:
-        return jsonify(account.save().to_dict())
+        return account.save().to_dict()
     except Exception as e:
         current_app.logger.error(e)
         abort(400, "could not create account")
@@ -30,7 +30,7 @@ def update_account():
         abort(401, "invalid account credentials")
 
     new_details = data['new_details']
-    return jsonify(account.update(**new_details).to_dict())
+    return account.update(**new_details).to_dict()
 
 
 @bp.route("/", methods=["DELETE"])
